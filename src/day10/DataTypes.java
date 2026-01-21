796b42a43917c0bf86225a754addcaa3ed860498
Ashish Mishra <ashish.mishra.sse@gmail.com>
2026-01-21 09:17:55 +0530

    Data Types Concepts

---
+++ src/day10/DataTypes.java
@@ -0,0 +1,47 @@
+package day10;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+         //dataType variableName = value;
+         //10
+        int x = 10;  //byte, short, int, long
+
+        //100
+
+        int y=100;
+
+        //23.45;
+        double z =23.45;
+
+        //int m = 2147483648;
+
+        // 55;
+
+        byte p1 =127;
+        byte p2 =-12;
+
+        short m1= 32767;
+
+        // 120; {byte, short, int, long}
+                 //-128 to 127
+                 //-32768 to 32767
+
+        int s =2147483647;  //4 byte
+
+        long q =2147483648L;
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+// byte/short/int/long/float/double/char/boolean
