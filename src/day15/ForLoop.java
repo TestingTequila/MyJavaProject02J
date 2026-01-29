637226eadee842d3f9f04f4156361e80ff043309
Ashish Mishra <ashish.mishra.sse@gmail.com>
2026-01-29 09:07:03 +0530

    Loops-While & For loop

---
+++ src/day15/ForLoop.java
@@ -0,0 +1,99 @@
+package day15;
+
+public class ForLoop {
+    public static void main(String[] args) {
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+
+//
+//          for (int i=1;i<=10;i++)
+//          {
+//              System.out.println("Welcome to Hilton Hotel");
+//          }
+
+//          for(;true;)
+//          {
+//              System.out.println("Welcome to my Hotel");
+//          }
+
+
+        //WAP to print numbers from 1-10
+        //1,2,3,4,5,6,7,8,9,10
+//        System.out.println(1);
+//        System.out.println(2);
+//        System.out.println(3);
+//        System.out.println(4);
+//        System.out.println(5);
+//        System.out.println(6);
+//        System.out.println(7);
+//        System.out.println(8);
+//        System.out.println(9);
+//        System.out.println(10);
+
+
+//        for (int i = 1; i <= 10; i = i + 1) {
+//            System.out.println(i);
+//        }
+
+
+        // WAP to print all the numbers from 1 -50 but everytime we get a number which is divisible by 5, print hello
+
+
+//        for (int i = 1;i <= 50;i++) {
+//            System.out.println(i);//1
+//            if (i % 5 == 0) {
+//                System.out.println("Hello");
+//            }
+//        }
+
+        //WAP to print the numbers from 10 to 1
+
+//        for (int i = 10;i>0;i--)
+//        {
+//            System.out.println(i);//10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+//
+//        }
+
+        // 1, 5, 9
+
+//
+//        for (int i =1;i<=9;i = i+4 )
+//        {
+//            System.out.println(i);
+//        }
+
+
+        //WAP to print table of 2
+
+
+//        for (int i =2;i<=20;i=i+2)
+//        {
+//            System.out.println(i);//2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+//
+//        }
+
+
+        for (int i = 1;i<=10;i++)
+        {
+            System.out.println(2*i); //2,4, 6, 8, 10, 12, 14, 16, 18, 20
+
+        }
+
+
+        //Today's Deals, Prime Video, Registry, Gift Cards, Customer Service, Sell
+//        String tab1 = "Today's Deals";
+//        String tab2 = "Prime Video";
+//        String tab3 = "Registry";
+//        String tab4 = "Gift Cards";
+//        String tab5 = "Customer Service";
+//        String tab6 = "Sell";
+    }
+}
---
+++ src/day15/WhileLoop.java
@@ -0,0 +1,103 @@
+package day15;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+//        System.out.println("Welcome to Hilton Hotel");
+
+//          int i=1;
+//          while (i<=10)
+//          {
+//              System.out.println("Welcome to Hilton Hotel");
+//              i++;
+//          }
+
+//          while(true)
+//          {
+//              System.out.println("Welcome to my Hotel");
+//          }
+
+
+        //WAP to print numbers from 1-10
+        //1,2,3,4,5,6,7,8,9,10
+//        System.out.println(1);
+//        System.out.println(2);
+//        System.out.println(3);
+//        System.out.println(4);
+//        System.out.println(5);
+//        System.out.println(6);
+//        System.out.println(7);
+//        System.out.println(8);
+//        System.out.println(9);
+//        System.out.println(10);
+
+//        int i = 1;
+//        while (i <= 10) {
+//            System.out.println(i);//1
+//            i = i + 1; //i++;
+//        }
+
+
+        // WAP to print all the numbers from 1 -50 but everytime we get a number which is divisible by 5, print hello
+
+//        int i = 1;
+//        while (i <= 50) {
+//            System.out.println(i);//1
+//            if (i % 5 == 0) {
+//                System.out.println("Hello");
+//            }
+//            i++;
+//        }
+
+        //WAP to print the numbers from 10 to 1
+//        int i = 10;
+//        while (i>0)
+//        {
+//            System.out.println(i);//10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+//            i--;
+//        }
+
+        // 1, 5, 9
+
+//        int i =1;
+//        while (i<=9)
+//        {
+//            System.out.println(i);
+//            i = i+4;
+//        }
+
+
+        //WAP to print table of 2
+
+//        int i =2;
+//        while (i<=20)
+//        {
+//            System.out.println(i);//2, 4, 6, 8, 10, 12, 14, 16, 18, 20
+//            i=i+2;
+//        }
+
+//        int i = 1;
+//        while (i<=10)
+//        {
+//            System.out.println(2*i); //2,4, 6, 8, 10, 12, 14, 16, 18, 20
+//            i++;
+//        }
+
+
+        //Today's Deals, Prime Video, Registry, Gift Cards, Customer Service, Sell
+        String tab1 = "Today's Deals";
+        String tab2 = "Prime Video";
+        String tab3 = "Registry";
+        String tab4 = "Gift Cards";
+        String tab5 = "Customer Service";
+        String tab6 = "Sell";
+    }
+}
